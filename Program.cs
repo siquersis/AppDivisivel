531c2aa26572cfc8b4145abd2cba7042b36b2f75
Evandro Siqueira <evandro.siqueira@onlineapp.com.br>
2022-02-18 15:34:53 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AppDivisivel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = 2520;
+            int menor = 2520;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (num % i == 0 && menor > i)
+                    menor = i;                                
+            }
+
+            Console.WriteLine("O menor numero: " + menor);
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.Read();
+        }
+    }
+}
